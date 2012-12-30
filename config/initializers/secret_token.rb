9a33cdfb720a5983f8ab51e10292a3795030b66d
Sashko Stubailo <sashko@ubuntu.(none)>
2012-12-29 21:55:10 -0800

    Committing a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ultramarine::Application.config.secret_token = '0d846c74c18b87190baba43a1cd9c3dcd1cb7f0af87cb969b3c5d36a31ca370383ef203176b56516fd6b1f9a6bab283efb4723e7695ddc94ed6d26e48ef8fdae'
