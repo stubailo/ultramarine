3321cbc7e9803a76a074e83ac874042ab698d51a
Carolyn Zhang <czhang92@gmail.com>
2013-01-19 18:23:27 -0500

    converted layout erb to haml, minor formatting changes

--- app/assets/javascripts/comments.js.coffee
+++ app/assets/javascripts/comments.js.coffee
@@ -1,3 +1,23 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$ ->
+  $("form.new_comment").submit comment_submit_handler 
+  
+comment_submit_handler = (event) ->
+  form = $(this)
+  $.ajax
+    url: form.attr("action")
+    data: form.serialize()
+    dataType: "script"
+    type: "POST"
+    complete: (data) ->
+      new_comment = $("<div></div>")
+      new_comment.append(data.responseText)
+      new_comment.hide()
+      new_comment.find("form.new_comment").submit comment_submit_handler
+      form.after new_comment
+      new_comment.slideDown()
+  form.find("textarea").val ""
+  return false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130117105010) do
+ActiveRecord::Schema.define(:version => 20130118040125) do
 
   create_table "challenges", :force => true do |t|
     t.string   "name"
@@ -40,10 +40,14 @@ ActiveRecord::Schema.define(:version => 20130117105010) do
   create_table "locations", :force => true do |t|
     t.integer  "parent_id"
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.string   "lat"
     t.string   "lon"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "omniauth_associations", :force => true do |t|
