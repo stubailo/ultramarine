6b32cd4c64f8f77d5b8b83829d4de36f25a865dd
Sashko Stubailo <sashko@mit.edu>
2013-01-19 20:59:42 -0500

    Fixed challenge page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120005548) do
+ActiveRecord::Schema.define(:version => 20130120012533) do
 
   create_table "challenges", :force => true do |t|
     t.string   "name"
