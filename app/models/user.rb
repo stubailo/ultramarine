78021b7b720e46d420d2d42cb91e3f50572f6df8
Sashko Stubailo <sashko@mit.edu>
2013-01-19 20:33:23 -0500

    Made challenge page sexy and added usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,17 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :omniauthable, :authentication_keys => [:login]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login
   # attr_accessible :title, :body
 
   has_many :omniauth_associations
 
   has_many :photos
+
+  attr_accessor :login
 
   def self.find_or_create_by_omniauth(auth)
     oa = OmniauthAssociation.where( {provider: auth.provider, uid: auth.uid} ).first
@@ -20,12 +22,21 @@ class User < ActiveRecord::Base
     if oa
       u = oa.user
     else
-      u = User.create(email:auth.info.email, password:Devise.friendly_token[0,20])
+      u = User.create(email:auth.info.email, username:auth.info.name, password:Devise.friendly_token[0,20])
       u.omniauth_associations = [OmniauthAssociation.new( {provider: auth.provider, uid: auth.uid} )]
       u.save
     end
 
     u
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", {:value => login.downcase}]).first
+    else
+      where(conditions).first
+    end
   end
 
   def admin?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130119232523) do
+ActiveRecord::Schema.define(:version => 20130120005548) do
 
   create_table "challenges", :force => true do |t|
     t.string   "name"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20130119232523) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.boolean  "is_admin"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
