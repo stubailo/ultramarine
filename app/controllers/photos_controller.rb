91ef2a0b7020fcac1e076fd3af30f71ef47e7e3a
Brodrick <brodrick.childs@gmail.com>
2013-01-25 03:27:30 -0500

    Added some links (edits, titles) and styled some forms (for photo editing)

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -98,10 +98,11 @@ class PhotosController < ApplicationController
   # DELETE /photos/1
   # DELETE /photos/1.json
   def destroy
+    challenge = @photo.challenge
     @photo.destroy
 
     respond_to do |format|
-      format.html { redirect_to photos_url }
+      format.html { redirect_to challenge }
       format.json { head :no_content }
     end
   end
