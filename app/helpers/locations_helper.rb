dcc194a4553b81ba46bccc0748368c64400ac963
Sashko Stubailo <sashko@mit.edu>
2013-01-26 23:08:05 -0500

    Modified sorting code

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,6 +3,7 @@ module LocationsHelper
     param_value ||= parameter
     direction = (param_value == sort_column && sort_direction == "desc") ? "asc" : "desc"
     css_class = (param_value == sort_column) ? "current #{sort_direction}" : nil
-    link_to parameter.titleize, {:sort => param_value, :direction => direction}, {:class => css_class}
+    content = parameter.titleize + "<i class='icon-caret-down'></i>" + "<i class='icon-caret-up'></i>"
+    link_to content.html_safe, {:sort => param_value, :direction => direction}, {:class => css_class}
   end
 end
