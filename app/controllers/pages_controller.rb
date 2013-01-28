54d597270c7482783f3205911fbe803b2f61e7d5
Brodrick <brodrick.childs@gmail.com>
2013-01-28 01:43:29 -0500

    Started work on logged in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
 class PagesController < ApplicationController
   skip_authorization_check
   def index
+    if current_user
+      @friend_ids = current_user.friend_ids(current_user, graph)
+      @challenges_todo = current_user.todos
+      @friend_completions = ChallengeCompletion.where(:user_id => @friend_ids)
+      @friend_challenges = []
+      @popular_challenges = Challenge.order("vote_value DESC")
+      @popular_challenges = @popular_challenges - @challenges_todo
+      @friend_completions.each do |completion|
+        challenge = Challenge.find(completion.challenge_id)
+        if !@challenges_todo.include?(challenge) && !@popular_challenges.include?(challenge)
+          @friend_challenges.append({:challenge => challenge, :friend => User.find(completion.user_id)})
+        end
+      end
+    end
   end
 
   def about
