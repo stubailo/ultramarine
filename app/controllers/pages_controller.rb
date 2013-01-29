45770b78899da1b41bd85866dbc08824e556ab86
Sashko Stubailo <sashko@mit.edu>
2013-01-29 13:23:16 -0500

    IDK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,14 +13,42 @@ class PagesController < ApplicationController
       @challenges_todo = current_user.todos
       @friend_completions = ChallengeCompletion.where(:user_id => @friend_ids)
       @friend_challenges = []
-      @popular_challenges = Challenge.order("vote_value DESC")
-      @popular_challenges = @popular_challenges - @challenges_todo - current_user.completed_challenges
       @friend_completions.each do |completion|
         challenge = Challenge.find(completion.challenge_id)
-        if !@challenges_todo.include?(challenge) && !@popular_challenges.include?(challenge)
+        if !@challenges_todo.include?(challenge)
           @friend_challenges.append({:challenge => challenge, :friend => User.find(completion.user_id)})
         end
       end
+      @popular_challenges = Challenge.order("vote_value DESC")
+      @popular_challenges = @popular_challenges - @friend_completions - @challenges_todo - current_user.completed_challenges
+      
+
+      @newsfeed = NewsfeedItem.joins(:user).where("newsfeed_items.user_id" => @friend_ids).order("newsfeed_items.created_at DESC").limit(100)
+      
+      @newsfeed.select! do |item|
+        if item.related_object_type == "Photo"
+          p = Photo.find(item.related_object_id)
+          if p.privacy_level == 1
+            next
+          end
+        elsif item.related_object_type == "Comment"
+          c = Comment.find(item.related_object_id)
+          base_comment = c
+          while base_comment.parent_id
+            base_comment = c.parent
+          end
+          if base_comment.photo_id
+            p = Photo.find(base_comment.photo_id)
+            if p.privacy_level == 1
+              next
+            end
+          end
+        end
+        true
+      end
+
+      @newsfeed = @newsfeed[0...15]
+
     else
       redirect_to root_path
     end
